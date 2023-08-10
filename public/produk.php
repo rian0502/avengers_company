2c56d8cc0814447c236b643ca7bb97958b6fed0a
RendyLutfiPrabowo <81194811+rendylutfiprabowo@users.noreply.github.com>
2023-08-10 21:04:42 +0700

    Update produk.php
    nambahin produk aje

--- public/produk.php
+++ public/produk.php
@@ -2,43 +2,30 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>AvengerNet || Produk</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>AvengerNet || Home </title>
     <!-- Favicon-->
     <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
     <!-- Bootstrap icons-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet" />
     <!-- Core theme CSS (includes Bootstrap)-->
     <link href="css/styles.css" rel="stylesheet" />
-    <style>
-        body{
-            
-        }
-        .container1 {
-            max-width: 1200px;
-            margin: 0 auto;
-            padding: 20px;
-        }
-
-        .product {
-            border: 1px solid #ccc;
-            padding: 10px;
-            margin: 10px;
-            background-color: white;
-        }
-    </style>
 </head>
 
 <body>
     <!-- Responsive navbar-->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container px-5">
-            <a class="navbar-brand" href="#!">ES TEH!</a>
+
+            <a class="navbar-brand" href="#!">Avengers Net</a>
+
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <li class="nav-item"><a class="nav-link"  href="..\public\index.php">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="..\public\index.php">Home</a></li>
                     <li class="nav-item"><a class="nav-link" href="#!">Paket & Promo</a></li>
                     <li class="nav-item"><a class="nav-link" href="#!">Contact</a></li>
                     <li class="nav-item"><a class="nav-link active" aria-current="page" href="..\public\produk.php">Produk</a></li>
@@ -46,27 +33,63 @@
             </div>
         </div>
     </nav>
-    <div class="container1">
-        <h1>Check it out!</h1>
 
-        <?php
-        // Daftar produk es teh
-        $products = array(
-            array("Es Teh Manis", "Minuman teh manis dengan es batu.", 15000),
-            array("Es Teh Tawar", "Minuman teh tawar dengan es batu.", 12000),
-            array("Es Teh Lemon", "Minuman teh dengan perasan lemon dan es batu.", 18000)
-        );
 
-        // Menampilkan daftar produk
-        foreach ($products as $product) {
-            echo '<div class="product">';
-            echo '<h2>' . $product[0] . '</h2>';
-            echo '<p>' . $product[1] . '</p>';
-            echo '<p>Harga: Rp ' . number_format($product[2], 0, ',', '.') . '</p>';
-            echo '</div>';
-        }
-        ?>
-    </div>
+    <!-- Pricing section-->
+    <section class="bg-light py-5 border-bottom">
+        <div class="container px-5 my-5">
+            <div class="text-center mb-5">
+                <h2 class="fw-bolder">Produk</h2>
+                <p class="lead mb-0">Produk & Layanan Avenger Net</p>
+            </div>
+            <div class="row gx-5 justify-content-center">
+                <!-- Pricing card free-->
+                <div class="col-lg-6 col-xl-4">
+                    <div class="card mb-5 mb-xl-0">
+                        <div class="card-body p-5">
+                            <div class="small text-uppercase fw-bold text-muted">Jasa Akses Internet</div>
+                            <div class="mb-3">
+                                <span class="display-4 fw-bold">Wifi</span>
+                            </div>
+                            <p>Layanan internet murah dengan kualitas kencang pertama di Konoha.</p>
+                            <br>
+                            <p>Akses internet sekarang jadi sangat murah dan cepat! Ambil <b>promo merdeka</b> sekarang juga!</p>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-xl-4">
+                    <div class="card mb-5 mb-xl-0">
+                        <div class="card-body p-5">
+                            <div class="small text-uppercase fw-bold text-muted">Layanan Televisi Digital</div>
+                            <div class="mb-3">
+                                <span class="display-4 fw-bold">Tv Kabel</span>
+                            </div>
+                            <p>Nonton tv, tapi siaran kemarin? TV kabel solusinya!</p>
+                            <br>
+                            <p>Nonton tv sekarang juga sudah bisa nonton ulang, bukan yt bae! <br>hahaa</p>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+    </section>
+    <!-- Footer-->
+    <footer class="py-5 bg-dark">
+        <div class="container px-5">
+            <p class="m-0 text-center text-white">Copyright &copy; Your Website 2023</p>
+        </div>
+    </footer>
+    <!-- Bootstrap core JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Core theme JS-->
+    <script src="js/scripts.js"></script>
+    <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+    <!-- * *                               SB Forms JS                               * *-->
+    <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+    <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+    <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
 </body>
 
 </html>
