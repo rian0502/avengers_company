0025a3843002a51d3c5303eb66f8f747f0e0ea10
Muhammad Febrian Hasibuan <muhammadfebrianhasibuan@gmail.com>
2023-08-11 02:12:49 +0700

    benerin link pada header

--- public/index.php
+++ public/index.php
@@ -20,7 +20,7 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container px-5">
 
-            <a class="navbar-brand" href="#!">ES TEH</a>
+            <a class="navbar-brand" href="/">Avengers Net</a>
 
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -29,7 +29,7 @@
                     <li class="nav-item"><a class="nav-link" href="#!">Paket & Promo</a></li>
                     <li class="nav-item"><a class="nav-link" href="#!">Contact</a></li>
                     <li class="nav-item"><a class="nav-link" href="/produk.php">Produk</a></li>
-                    <li class="nav-item"><a class="nav-link" href="/help.php">Produk</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/help.php">Bantuan</a></li>
                 </ul>
             </div>
         </div>
