8c7745f4205bf1d5cdd06275453d050187787d4d
Muhammad Febrian Hasibuan <muhammadfebrianhasibuan@gmail.com>
2023-08-11 02:03:36 +0700

    benerin link pada header

--- public/index.php
+++ public/index.php
@@ -28,7 +28,8 @@
                     <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a></li>
                     <li class="nav-item"><a class="nav-link" href="#!">Paket & Promo</a></li>
                     <li class="nav-item"><a class="nav-link" href="#!">Contact</a></li>
-                    <li class="nav-item"><a class="nav-link" href="..\public\produk.php">Produk</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/produk.php">Produk</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/help.php">Produk</a></li>
                 </ul>
             </div>
         </div>
